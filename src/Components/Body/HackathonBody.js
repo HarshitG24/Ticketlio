0172c669d638eebfceb9b3167cb0867fe5691384
Harshit Gajjar <hrstgajjar3@gmail.com>
2022-03-08 13:48:27 -0800

    added content for hackathon page

--- src/Components/Body/HackathonBody.js
+++ src/Components/Body/HackathonBody.js
@@ -17,22 +17,21 @@ const hackathonData = [
   {
     title: "NYUAD Hackathon",
     description:
-      "A hackathon is a design sprint-like event, in which often computer programmers and others involved in software development, including graphic designers, interface designers, product managers, project managers, domain experts, and others collaborate intensively on software projects.",
-    duration: "2 Days",
-    date: "March 15, 2022 to March 16, 2022",
+      "We are excited to announce the 10th annual NYUAD Hackathon for Social Good which will focus on one of the most promising technologies of our time, Quantum Computing (QC). We will form cross-disciplinary global teams building solutions to challenging problems we are facing today using Quantum Computing technologies.",
+    duration: "6 Days",
+    date: "April 21, 2022 to March 26, 2022",
     location: "San Francisco",
-    posterUrl:
-      "https://mir-s3-cdn-cf.behance.net/project_modules/fs/f35ee593213265.5e5f0394bc88b.jpg",
+    posterUrl: "https://pbs.twimg.com/media/EVqzx6bX0AAKGmj.jpg",
   },
   {
-    title: "NEU Hackathon",
+    title: "Code Hackathon",
     description:
       "A hackathon is a design sprint-like event, in which often computer programmers and others involved in software development, including graphic designers, interface designers, product managers, project managers, domain experts, and others collaborate intensively on software projects.",
-    duration: "2 Days",
-    date: "March 15, 2022 to March 16, 2022",
+    duration: "1 Day",
+    date: "May 5, 2022 to May 6, 2022",
     location: "San Francisco",
     posterUrl:
-      "https://mir-s3-cdn-cf.behance.net/project_modules/fs/f35ee593213265.5e5f0394bc88b.jpg",
+      "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/code-hackathon-event-poster-design-template-d7c8ddfd12e11fd2b9c6b70241626091_screen.jpg?ts=1567076214",
   },
 ];
 export default class HackathonBody extends Component {
