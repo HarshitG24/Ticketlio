6c555b3c66e6dd321846f6954d31285ba493c8f6
Harshit Gajjar <hrstgajjar3@gmail.com>
2022-03-07 22:50:25 -0800

    modified hackathon page

--- src/Components/Body/Hackathon.js
+++ src/Components/Body/Hackathon.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
+import "./CSS/Hackathon.css"
+import HackathonBody from "./HackathonBody";
 
 export default class Hackathon extends Component {
   render() {
     return (
-      <div>
+      <div className="hackathon">
         <img
-          src="https://blog.webex.com/wp-content/uploads/2021/03/Announcing-the-winners-of-Collaboration-hackathon-Powering-the-next-era-of-innovation-scaled.jpg"
+          src="https://t3.ftcdn.net/jpg/03/01/13/20/360_F_301132090_LKoSp3l3cXlCo78zaAe2M9LI2z5yznvB.jpg"
           alt="new"
           className="hackathon-poster"
         />
-        <h1>Hackathon</h1>
+        <div>
+          <h1>Hackathon</h1>
+          <HackathonBody />
+        </div>
       </div>
     );
   }
