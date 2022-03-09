093b97ee5e2e4eef0dddff2381b5bb2bee53b370
Harshit Gajjar <hrstgajjar3@gmail.com>
2022-03-09 15:21:22 -0800

    added book ticket functionality in sports page

--- src/Components/Body/SportCategory.js
+++ src/Components/Body/SportCategory.js
@@ -5,14 +5,22 @@ import "./CSS/SportMatches.css";
 export default class SportCategory extends Component {
     render() {
       const {category, matches} = this.props;
+      console.log("matches", matches, this.props.city);
+      let arr = matches.filter(elem => elem.location === this.props.city);
         return (
           <div className="sport-category-group">
             <p className="sport-name left-space">{category}</p>
-            <div className="group-sports">
-              {matches.map((elem) => {
-                return <SportMatches mData={elem} />;
-              })}
-            </div>
+            {arr.length > 0 ? (
+              <div>
+                <div className="group-sports">
+                  {arr.map((elem) => {
+                    return <SportMatches mData={elem} />;
+                  })}
+                </div>
+              </div>
+            ) : (
+              <h5 style={{textAlign: "center", marginBottom: "4rem", "marginTop": "2rem"}}>No Matches Found</h5>
+            )}
           </div>
         );
     }
