4e9afabf512a454bf6757dd3c98029b62cfb5446
Harshit Gajjar <hrstgajjar3@gmail.com>
2022-03-12 23:11:44 -0500

    updated UI for register events page

--- src/Components/Register Events/Events.js
+++ src/Components/Register Events/Events.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import "./Events.css"
+import {cities} from "../../Data/Data"
 
 export default class Events extends Component {
     constructor(props) {
@@ -18,14 +19,44 @@ export default class Events extends Component {
     }
 
     onInputFieldChange(evt, key){
+
+      console.log("value is", evt.target.value);
         this.setState({
             [key]: evt.target.value
         })
+    }
 
-        // localStorage.setItem(
-        //   "registration-form",
-        //   `{name: ${evt.target.value}}`
-        // );
+    onGetDate(evt, key){
+      let dateVal =  evt.target.value;
+      dateVal =  dateVal.split('-');
+
+      let date = dateVal[2];
+      let month = this.generateMonth(dateVal[1]);
+      let year = dateVal[0];
+
+      this.setState({
+        [key]: `${date} ${month}, ${year}`
+      })
+      
+    }
+
+    generateMonth(key){
+      let arr = [
+        'January',
+        'Febraury',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+      ]
+
+      return arr[parseInt(key)];
     }
 
 
@@ -43,94 +74,140 @@ export default class Events extends Component {
         hackData.push(obj);
 
         localStorage.setItem("hackathon-data", JSON.stringify(hackData));
+
+        this.setState({
+          title: "",
+          description: "",
+          duration: "",
+          date: "",
+          location: "",
+          posterUrl: "",
+        });
+
+        alert('Thank you for registering the event, we have listed it for the users')
+    }
+
+    disableSubmitBtn(){
+      const {
+            name,
+            description,
+            fromDate,
+            toDate,
+            location,
+            poster,
+            duration,
+      } = this.state;
+
+      return (name === "") && (description === "") && (fromDate === "") && (toDate === "") && (location === "") && (poster === "") && (duration === "")
     }
 
     render() { 
+      let dis = this.disableSubmitBtn();
         return (
-          <div
-            style={{
-              width: "100%",
-              height: "80vh",
-              display: "flex",
-              flexDirection: "row",
-            }}
-          >
-            <div style={{ width: "50%", height: "100%" }}>
-              <img
-                src="https://www.nicepng.com/png/full/335-3355144_seller-registration-icon-class-register-icon-png.png"
-                alt="new"
-                className="left-img"
-              />
-            </div>
-            <div style={{ width: "50%", height: "100%" }}>
-              <h1 className="page-title">Register for event</h1>
+          <div className="events">
+            <div className="title-div">
+              <div>
+                <h1 className="page-title">Register for event</h1>
+              </div>
 
-              <div style={{ margin: 0 }}>
-                <p>Name of the event</p>
+              <div className="option-div">
+                <p className="option-p">Name of the event</p>
                 <input
                   type="text"
                   id="name"
                   name="name"
+                  className="event-input"
                   value={this.state.name}
                   onChange={(evt) => this.onInputFieldChange(evt, "name")}
                 />
               </div>
 
-              <p>Description</p>
-              <input
-                type="text"
-                id="description"
-                name="description"
-                value={this.state.description}
-                onChange={(evt) => this.onInputFieldChange(evt, "description")}
-              />
+              <div className="option-div">
+                <p className="option-p">Description</p>
+                <input
+                  type="text"
+                  id="description"
+                  name="description"
+                  className="event-input"
+                  value={this.state.description}
+                  onChange={(evt) =>
+                    this.onInputFieldChange(evt, "description")
+                  }
+                />
+              </div>
 
-              <p>From Date</p>
-              <input
-                type="text"
-                id="fromDate"
-                name="fromDate"
-                value={this.state.fromDate}
-                onChange={(evt) => this.onInputFieldChange(evt, "fromDate")}
-              />
+              <div className="option-div">
+                <p className="option-p">From Date</p>
+                <input
+                  type="date"
+                  id="fromDate"
+                  name="fromDate"
+                  className="event-input"
+                  value={this.state.fromDate}
+                  onChange={(evt) => this.onInputFieldChange(evt, "fromDate")}
+                />
+              </div>
 
-              <p>To Date</p>
-              <input
-                type="text"
-                id="toDate"
-                name="toDate"
-                value={this.state.toDate}
-                onChange={(evt) => this.onInputFieldChange(evt, "toDate")}
-              />
+              <div className="option-div">
+                <p className="option-p">To Date</p>
+                <input
+                  type="date"
+                  id="toDate"
+                  name="toDate"
+                  className="event-input"
+                  value={this.state.toDate}
+                  onChange={(evt) => this.onInputFieldChange(evt, "toDate")}
+                />
+              </div>
 
-              <p>To Date</p>
-              <input
-                type="text"
-                id="duration"
-                name="duration"
-                value={this.state.duration}
-                onChange={(evt) => this.onInputFieldChange(evt, "duration")}
-              />
+              <div className="option-div">
+                <p className="option-p">Number of Days</p>
+                <input
+                  type="text"
+                  id="duration"
+                  name="duration"
+                  className="event-input"
+                  value={this.state.duration}
+                  onChange={(evt) => this.onInputFieldChange(evt, "duration")}
+                />
+              </div>
 
-              <p>Location</p>
-              <input
-                type="text"
-                id="location"
-                name="location"
-                value={this.state.location}
-                onChange={(evt) => this.onInputFieldChange(evt, "location")}
-              />
+              <div className="option-div">
+                <p className="option-p">Location</p>
+                <select
+                  name="cityList"
+                  id="cityList"
+                  value={this.state.location}
+                  className="city-select"
+                  onChange={(evt) => this.onInputFieldChange(evt, "location")}
+                >
+                  {cities.map((e) => {
+                    return <option value={e}>{e}</option>;
+                  })}
+                </select>
+              </div>
 
-              <p>Poster URL</p>
-              <input
-                type="text"
-                id="poster"
-                name="poster"
-                value={this.state.poster}
-                onChange={(evt) => this.onInputFieldChange(evt, "poster")}
-              />
+              <div className="option-div">
+                <p className="option-p">Poster URL</p>
+                <input
+                  type="text"
+                  id="poster"
+                  name="poster"
+                  className="event-input"
+                  value={this.state.poster}
+                  onChange={(evt) => this.onInputFieldChange(evt, "poster")}
+                />
+              </div>
 
-              <button onClick={() => this.onSubmitForm()}>Submit</button>
+              <button
+                className={
+                  dis ? "submit-event-btn-disabled" : "submit-event-btn"
+                }
+                onClick={() => this.onSubmitForm()}
+                disabled={dis}
+              >
+                Submit
+              </button>
             </div>
           </div>
         );
